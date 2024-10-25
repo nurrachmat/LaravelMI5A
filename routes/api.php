a66eadf9841555a9681d8cc895dd7a7465361679
Nur Rachmat <>
2024-10-25 15:00:49 +0700

    API Delete

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ Route::get('mahasiswa', [MahasiswaController::class, 'getMahasiswa']);
 
 Route::post('fakultas', [FakultasController::class, 'storeFakultas']);
 Route::post('prodi', [ProdiController::class, 'storeProdi']);
+
+Route::delete('fakultas/{id}', [FakultasController::class, 'destroyFakultas']);
